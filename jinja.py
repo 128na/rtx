7c633a44d3d90673c7c8e556babf16ac986384dd
128na <simutrans.128na@gmail.com>
2024-08-18 00:40:55 +0900

    wip

--- jinja.py
+++ jinja.py
@@ -17,5 +17,32 @@ if __name__ == "__main__":
 
     template = env.get_template(args.file_name)
 
-    data = {}
+    data = {
+        "project": "RTX",
+        "defs": [
+            {
+                "mode": "road",
+                "types": [30, 50, 120],
+                "system_types": ["G", "E"],
+                "layouts": ["F", "C", "B", "S"],
+            },
+            {
+                "mode": "texture",
+                "types": [1, 2, 3, 4, 5],
+                "system_types": ["G"],
+                "layouts": ["Ti"],
+            },
+        ],
+        "meta": {
+            "types": {
+                30: {"cost": 15000, "maintenance": 300},
+                50: {"cost": 25000, "maintenance": 500},
+                120: {"cost": 100000, "maintenance": 2000},
+            },
+            "system_types": {
+                "G": {"id": 0, "cost": 1},
+                "E": {"id": 1, "cost": 5},
+            },
+        },
+    }
     print(template.render(data))
