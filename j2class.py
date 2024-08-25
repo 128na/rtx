ad957e52a8abe4895d5d104326ef852099d50e8a
128na <simutrans.128na@gmail.com>
2024-08-25 15:47:34 +0900

    wayobj版舗装を追加

--- j2class.py
+++ j2class.py
@@ -104,42 +104,24 @@ class RTGSidewalk(RTG):
     def get_dest_path(self):
         return f"sidewalk_{ self.get_name() }_{ self.d["layout"] }"
 
+    def is_full_tile(self):
+        return self.d["layout"] == "A"
+
+    def get_source_location(self, loc: str):
+        if self.is_full_tile():
+            return "0.0"
+        return loc
+
     def get_icons(self):
         return [
             {
-                "suffix": "_f",
+                "suffix": "",
                 "source": "0.0",
                 "dest": "0.0",
                 "dest_cur": "2.0",
                 "converts": ["to_n"],
             },
-            {
-                "suffix": "_c",
-                "source": "0.0",
-                "dest": "0.2",
-                "dest_cur": "2.2",
-                "converts": ["to_n"],
-            },
-            {
-                "suffix": "_b",
-                "source": "0.0",
-                "dest": "0.4",
-                "dest_cur": "2.4",
-                "converts": ["to_n"],
-            },
-            {
-                "suffix": "_s",
-                "source": "0.0",
-                "dest": "0.6",
-                "dest_cur": "2.6",
-                "converts": ["to_n"],
-            },
         ]
-
-    def get_source_location(self, loc: str):
-        if self.d["layout"] == "A":
-            return "0.0"
-        return loc
 
 
 class Merge:
@@ -184,6 +166,8 @@ class MergeSidewalk(Merge):
     def get_dest_path(self):
         return f"sidewalk_{ self.get_name() }"
 
+    def is_texture(self):
+        return True
 
 class Dat:
     name: str
@@ -317,8 +301,21 @@ class DatGTX(Dat):
 class DatTi(Dat):
     name = "Ti"
 
+    def is_way_obj(self):
+        return self.d["way_type"] != "T"
+
+    def get_waytype(self):
+        if self.is_way_obj():
+            return "road"
+        return "tram_track"
+
+    def get_obj(self):
+        if self.is_way_obj():
+            return "way-object"
+        return "way"
+        
     def get_full_name(self):
-        return f"{self.name} {self.get_name()}"
+        return f"{self.name} {self.get_name()}{self.d["way_type"]}"
 
     def get_name(self):
         match self.d["speed"]:
@@ -331,9 +328,6 @@ class DatTi(Dat):
 
     def get_system_type(self):
         return 0
-
-    def get_waytype(self):
-        return "tram_track"
 
     def get_topspeed(self):
         return self.d["speed"]
@@ -350,36 +344,43 @@ class DatTi(Dat):
     def get_image_path(self):
         return f"texture_{ self.d["texture"] }"
 
+    def get_image_name(self, name):
+        if self.is_way_obj():
+            return f"back{name}"
+        return name
+
     def get_images(self):
+        name = self.get_image_name
+        path = self.get_image_path
         return [
-            ["image", "ns", self.get_image_path(), 0, 2, -64, -96],
-            ["image", "ew", self.get_image_path(), 0, 3, -64, -96],
-            ["image", "s", self.get_image_path(), 0, 5, -64, -96],
-            ["image", "w", self.get_image_path(), 0, 6, -64, -96],
-            ["image", "n", self.get_image_path(), 0, 7, -64, -96],
-            ["image", "e", self.get_image_path(), 0, 8, -64, -96],
-            ["image", "-", self.get_image_path(), 0, 9, -64, -96],
-            ["imageup", "12", self.get_image_path(), 1, 0, -64, -88],
-            ["imageup", "9", self.get_image_path(), 1, 1, -64, -88],
-            ["imageup", "3", self.get_image_path(), 1, 2, -64, -88],
-            ["imageup", "6", self.get_image_path(), 1, 3, -64, -88],
-            ["imageup2", "12", self.get_image_path(), 1, 5, -64, -80],
-            ["imageup2", "9", self.get_image_path(), 1, 6, -64, -80],
-            ["imageup2", "3", self.get_image_path(), 1, 7, -64, -80],
-            ["imageup2", "6", self.get_image_path(), 1, 8, -64, -80],
-            ["diagonal", "sw", self.get_image_path(), 2, 0, -64, -96],
-            ["diagonal", "nw", self.get_image_path(), 2, 1, -64, -96],
-            ["diagonal", "ne", self.get_image_path(), 2, 2, -64, -96],
-            ["diagonal", "se", self.get_image_path(), 2, 3, -64, -96],
-            ["image", "sw", self.get_image_path(), 2, self.get_curve_x(0), -64, -96],
-            ["image", "nw", self.get_image_path(), 2, self.get_curve_x(1), -64, -96],
-            ["image", "ne", self.get_image_path(), 2, self.get_curve_x(2), -64, -96],
-            ["image", "se", self.get_image_path(), 2, self.get_curve_x(3), -64, -96],
-            ["image", "new", self.get_image_path(), 4, 0, -64, -96],
-            ["image", "nse", self.get_image_path(), 4, 1, -64, -96],
-            ["image", "sew", self.get_image_path(), 4, 2, -64, -96],
-            ["image", "nsw", self.get_image_path(), 4, 3, -64, -96],
-            ["image", "nsew", self.get_image_path(), 4, 4, -64, -96],
+            [name("image"), "ns", path(), 0, 2, -64, -96],
+            [name("image"), "ew", path(), 0, 3, -64, -96],
+            [name("image"), "s", path(), 0, 5, -64, -96],
+            [name("image"), "w", path(), 0, 6, -64, -96],
+            [name("image"), "n", path(), 0, 7, -64, -96],
+            [name("image"), "e", path(), 0, 8, -64, -96],
+            [name("image"), "-", path(), 0, 9, -64, -96],
+            [name("imageup"), "12", path(), 1, 0, -64, -88],
+            [name("imageup"), "9", path(), 1, 1, -64, -88],
+            [name("imageup"), "3", path(), 1, 2, -64, -88],
+            [name("imageup"), "6", path(), 1, 3, -64, -88],
+            [name("imageup2"), "12", path(), 1, 5, -64, -80],
+            [name("imageup2"), "9", path(), 1, 6, -64, -80],
+            [name("imageup2"), "3", path(), 1, 7, -64, -80],
+            [name("imageup2"), "6", path(), 1, 8, -64, -80],
+            [name("diagonal"), "sw", path(), 2, 0, -64, -96],
+            [name("diagonal"), "nw", path(), 2, 1, -64, -96],
+            [name("diagonal"), "ne", path(), 2, 2, -64, -96],
+            [name("diagonal"), "se", path(), 2, 3, -64, -96],
+            [name("image"), "sw", path(), 2, self.get_curve_x(0), -64, -96],
+            [name("image"), "nw", path(), 2, self.get_curve_x(1), -64, -96],
+            [name("image"), "ne", path(), 2, self.get_curve_x(2), -64, -96],
+            [name("image"), "se", path(), 2, self.get_curve_x(3), -64, -96],
+            [name("image"), "new", path(), 4, 0, -64, -96],
+            [name("image"), "nse", path(), 4, 1, -64, -96],
+            [name("image"), "sew", path(), 4, 2, -64, -96],
+            [name("image"), "nsw", path(), 4, 3, -64, -96],
+            [name("image"), "nsew", path(), 4, 4, -64, -96],
         ]
 
     def get_curve_x(self, x: int):
@@ -391,7 +392,7 @@ class DatTi(Dat):
 
 class DatTiSingle(DatTi):
     def get_full_name(self):
-        return f"{self.name} {self.get_name()} {self.d["layout"]}"
+        return f"{self.name} {self.get_name()} {self.d["layout"]}{self.d["way_type"]}"
 
     def get_image_path(self):
         """
@@ -407,14 +408,22 @@ class DatTiSingle(DatTi):
         return f"texture_{self.d["texture"]}"
 
     def get_images(self):
+        name = self.get_image_name
+        path = self.get_image_path
         images = super().get_images()
         images[4:6] = [
-            ["image", "n", self.flip(), 0, 7, -64, -96],
-            ["image", "e", self.flip(), 0, 8, -64, -96],
+            [name("image"), "n", self.flip(), 0, 7, -64, -96],
+            [name("image"), "e", self.flip(), 0, 8, -64, -96],
         ]
-        images[19:21] = [
-            ["image", "ne", self.flip(), 2, self.get_curve_x(2), -64, -96],
-            ["image", "se", self.flip(), 2, self.get_curve_x(3), -64, -96],
+        images[15:23] = [
+            [name("diagonal"), "sw", path(), 2, 0, -64, -96],
+            [name("diagonal"), "nw", path(), 2, 1, -64, -96],
+            [name("diagonal"), "ne", self.flip(), 2, 2, -64, -96],
+            [name("diagonal"), "se", self.flip(), 2, 3, -64, -96],
+            [name("image"), "sw", path(), 2, self.get_curve_x(0), -64, -96],
+            [name("image"), "nw", path(), 2, self.get_curve_x(1), -64, -96],
+            [name("image"), "ne", self.flip(), 2, self.get_curve_x(2), -64, -96],
+            [name("image"), "se", self.flip(), 2, self.get_curve_x(3), -64, -96],
         ]
         return images
 
@@ -447,201 +456,35 @@ class DatFX(DatTi):
         return name
 
     def get_images(self):
+        name = self.get_image_name
+        path = self.get_image_path
         return [
-            [self.get_image_name("image"), "ns", self.get_image_path(), 0, 2, -64, -96],
-            [self.get_image_name("image"), "ew", self.get_image_path(), 0, 3, -64, -96],
-            [self.get_image_name("image"), "s", self.get_image_path(), 0, 5, -64, -96],
-            [self.get_image_name("image"), "w", self.get_image_path(), 0, 6, -64, -96],
-            [self.get_image_name("image"), "n", self.get_image_path(), 0, 7, -64, -96],
-            [self.get_image_name("image"), "e", self.get_image_path(), 0, 8, -64, -96],
-            [self.get_image_name("image"), "-", self.get_image_path(), 0, 9, -64, -96],
-            [
-                self.get_image_name("imageup"),
-                "12",
-                self.get_image_path(),
-                1,
-                0,
-                -64,
-                -88,
-            ],
-            [
-                self.get_image_name("imageup"),
-                "9",
-                self.get_image_path(),
-                1,
-                1,
-                -64,
-                -88,
-            ],
-            [
-                self.get_image_name("imageup"),
-                "3",
-                self.get_image_path(),
-                1,
-                2,
-                -64,
-                -88,
-            ],
-            [
-                self.get_image_name("imageup"),
-                "6",
-                self.get_image_path(),
-                1,
-                3,
-                -64,
-                -88,
-            ],
-            [
-                self.get_image_name("imageup2"),
-                "12",
-                self.get_image_path(),
-                1,
-                5,
-                -64,
-                -80,
-            ],
-            [
-                self.get_image_name("imageup2"),
-                "9",
-                self.get_image_path(),
-                1,
-                6,
-                -64,
-                -80,
-            ],
-            [
-                self.get_image_name("imageup2"),
-                "3",
-                self.get_image_path(),
-                1,
-                7,
-                -64,
-                -80,
-            ],
-            [
-                self.get_image_name("imageup2"),
-                "6",
-                self.get_image_path(),
-                1,
-                8,
-                -64,
-                -80,
-            ],
-            [
-                self.get_image_name("diagonal"),
-                "sw",
-                self.get_image_path(),
-                2,
-                0,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("diagonal"),
-                "nw",
-                self.get_image_path(),
-                2,
-                1,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("diagonal"),
-                "ne",
-                self.get_image_path(),
-                2,
-                2,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("diagonal"),
-                "se",
-                self.get_image_path(),
-                2,
-                3,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("image"),
-                "sw",
-                self.get_image_path(),
-                self.get_sw_nw_y(),
-                5,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("image"),
-                "nw",
-                self.get_image_path(),
-                self.get_sw_nw_y(),
-                6,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("image"),
-                "ne",
-                self.get_image_path(),
-                self.get_ne_se_y(),
-                7,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("image"),
-                "se",
-                self.get_image_path(),
-                self.get_ne_se_y(),
-                8,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("image"),
-                "new",
-                self.get_image_path(),
-                4,
-                0,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("image"),
-                "nse",
-                self.get_image_path(),
-                4,
-                1,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("image"),
-                "sew",
-                self.get_image_path(),
-                4,
-                2,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("image"),
-                "nsw",
-                self.get_image_path(),
-                4,
-                3,
-                -64,
-                -96,
-            ],
-            [
-                self.get_image_name("image"),
-                "nsew",
-                self.get_image_path(),
-                4,
-                4,
-                -64,
-                -96,
-            ],
+            [name("image"), "ns", path(), 0, 2, -64, -96],
+            [name("image"), "ew", path(), 0, 3, -64, -96],
+            [name("image"), "s", path(), 0, 5, -64, -96],
+            [name("image"), "w", path(), 0, 6, -64, -96],
+            [name("image"), "n", path(), 0, 7, -64, -96],
+            [name("image"), "e", path(), 0, 8, -64, -96],
+            [name("image"), "-", path(), 0, 9, -64, -96],
+            [name("imageup"), "12", path(), 1, 0, -64, -88],
+            [name("imageup"), "9", path(), 1, 1, -64, -88],
+            [name("imageup"), "3", path(), 1, 2, -64, -88],
+            [name("imageup"), "6", path(), 1, 3, -64, -88],
+            [name("imageup2"), "12", path(), 1, 5, -64, -80],
+            [name("imageup2"), "9", path(), 1, 6, -64, -80],
+            [name("imageup2"), "3", path(), 1, 7, -64, -80],
+            [name("imageup2"), "6", path(), 1, 8, -64, -80],
+            [name("diagonal"), "sw", path(), 2, 0, -64, -96],
+            [name("diagonal"), "nw", path(), 2, 1, -64, -96],
+            [name("diagonal"), "ne", path(), 2, 2, -64, -96],
+            [name("diagonal"), "se", path(), 2, 3, -64, -96],
+            [name("image"), "sw", path(), self.get_sw_nw_y(), 5, -64, -96],
+            [name("image"), "nw", path(), self.get_sw_nw_y(), 6, -64, -96],
+            [name("image"), "ne", path(), self.get_ne_se_y(), 7, -64, -96],
+            [name("image"), "se", path(), self.get_ne_se_y(), 8, -64, -96],
+            [name("image"), "new", path(), 4, 0, -64, -96],
+            [name("image"), "nse", path(), 4, 1, -64, -96],
+            [name("image"), "sew", path(), 4, 2, -64, -96],
+            [name("image"), "nsw", path(), 4, 3, -64, -96],
+            [name("image"), "nsew", path(), 4, 4, -64, -96],
         ]
